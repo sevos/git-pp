c151003ae43e1d7c2998939854a309d4b7c91c3c
Artur Roszczyk <artur.roszczyk@gmail.com>
2012-11-10 13:05:27 +0100

    Working reading from config file

--- lib/git-pp.rb
+++ lib/git-pp.rb
@@ -2,5 +2,11 @@ require "git-pp/version"
 
 module Git
   module Pp
+    module Activable
+      def activate
+        `git config user.name '#{name}'`
+        `git config user.email '#{email}'`
+      end
+    end
   end
 end
